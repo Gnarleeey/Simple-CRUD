e8b43d4ea0a6060c70883551cee23b9ddeb783a9
JAO <soriano1820207@mkt.ceu.edu.ph>
2021-08-29 17:20:30 +0800

    Simple Crud

---
+++ resources/views/students/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Student Simple Crud</title>
+
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+	rel="stylesheet">
+</head>
+<body>
+
+	<div class="container">
+		@yield('content')
+
+	</div>
+
+</body>
+</html>
